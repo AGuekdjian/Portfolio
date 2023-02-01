a7a50d20bf3a079389d9f001a1340b86ee95b336
Anthony Guekdjian <anthonyguekdjian@gmail.com>
2023-02-01 18:28:48 -0300

    Se modifica el contenido de la web

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -13,17 +13,12 @@ const About = () => {
                 </div>
                 <div className='about__content'>
                     <p>
-                        Me gusta trabajar en equipos y proyectos diversos
-                        que me permitan aprender continuamente sobre
-                        diversas tecnologías, métodos y soluciones.
-                        Actualmente, estoy enfocado en la construcción
-                        de aplicaciones web. <br />
-                        Soy una persona que ama los desafíos, que no se
-                        rinde fácilmente, responsable, detallista y
-                        autodidacta. <br />
-                        Por eso he decidido dedicarme a la programación,
-                        ya que es un mundo donde se valoran estas
-                        capacidades.
+                        Soy Programador Web Front-end Jr y Back-end Jr en continuo aprendizaje, mi interés actual es sobre Front-end y estudiante de la carrera Certified Tech Developer de Digital House.<br />
+                        Certified Tech Developer es una iniciativa educativa co-creada por Digital House, Mercado Libre y Globant para formar Desarrolladores siguiendo los más altos estándares de la industria.
+                        El programa enseña los conocimientos técnicos y habilidades blandas que demandan las empresas más disruptivas del sector.<br />
+                        Me gusta trabajar en equipos y proyectos diversos que me permitan aprender continuamente sobre diversas tecnologías, métodos y soluciones.<br />
+                        Actualmente me desempeño como programador Front-end formando parte de un equipo freelance, enfocado en la construcción de aplicaciones web.<br />
+                        Estoy buscando formar parte de una empresa u organización formalmente para aportar mis habilidades, cumplir objetivos y seguir mejorando mis conocimientos.
                     </p>
                     <a href='#contact' className='btn ov-btn-slide-left'>Contactar</a>
                 </div>
--- src/components/header/CV.jsx
+++ src/components/header/CV.jsx
@@ -1,15 +1,14 @@
-import React, { useState } from 'react'
+import React from 'react'
 import CV from '../../assets/cv.pdf'
 
 const CVitae = () => {
 
-    // const [state, setState] = useState(false)
 
     return (
         <>
             <div className='cv'>
                 <a href={CV} download className='btn ov-btn-slide-left' >Descargar CV</a>
-                <a href='#contact' className='btn btn-primary' onClick={() => setState(true)}>Contactar</a>
+                <a href='#contact' className='btn btn-primary'>Contactar</a>
             </div>
         </>
     )
